c9e947937ea140820c33ad9f6bf5c36f038a4def
Taegon Kim <taegon.k@gmail.com>
2018-06-24 11:15:30 -0500

    Commit initial structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# wc2018
-Scoring our WC prediction
+# World Cup 2018
+Scoring our WC2018 predictions
+
+* Template file is [here](input/world_cup_2018.xlsx).
+
---
+++ wc2018/wc2018.py
@@ -0,0 +1,19 @@
+
+
+def read_final_score():
+    pass
+
+
+def read_scores():
+    pass
+
+
+def build_html():
+    pass
+
+def main():
+    read_final_score()
+
+
+if __name__ == "__main__":
+    main()
